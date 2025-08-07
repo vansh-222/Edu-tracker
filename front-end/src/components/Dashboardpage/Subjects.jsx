e1af74a6ad894ffba240cbf559f3147f88005f8b
vansh-222 <vanshnayyar54@gmail.com>
2025-08-08 00:12:05 +0530

    Initial commit

--- front-end/src/components/Dashboardpage/Subjects.jsx
+++ front-end/src/components/Dashboardpage/Subjects.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const API = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+export const getSubjects = () => API.get("/subjects");
+export const createSubject = (newSubject) => API.post("/subjects", newSubject);
+export const updateSubject = (id, updated) => API.put(`/subjects/${id}`, updated);
+export const deleteSubject = (id) => API.delete(`/subjects/${id}`);
+
 
 function Subjects() {
   const [name, setName] = useState("");
